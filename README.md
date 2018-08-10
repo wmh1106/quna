d4e94c0499b8f9b12e1b791d52b6c4fa4e902096
wmh1106 <406875886@qq.com>
2018-08-10 16:58:09 +0800

    项目初始化

--- README.md
+++ README.md
@@ -17,3 +17,17 @@ npm run build
 # build for production and view the bundle analyzer report
 npm run build --report
 ```
+
+## 页面
+
+### 1. 多页：
+
+1. 页面跳转返回 HTML
+2. 优点：首屏相对单页时间快、SEO 效果好
+3. 缺点：页面切换慢
+
+### 2. 单页
+
+1. 页面跳转：由 JS 渲染
+2. 优点：页面切换快速
+3. 首屏渲染时间慢、SEO差
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+
+// 重置样式
+import '@/assets/style/reset.css'
+// 1像素边框样式
+import '@/assets/style/border.css'
+// 移动端300毫秒延迟
+import fastClick from 'fastclick'
+
+fastClick.attach(document.body)
 
 Vue.config.productionTip = false
 
@@ -10,6 +17,8 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
